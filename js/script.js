af41ff4eae7028ed84fdd8c7a2209ea92cd0c1ac
feLycks <felix.obiero@student.moringaschool.com>
2021-11-08 11:19:41 +0300

    store users to local storage

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 function getValues() {
     // get values from suer 
+
+    // const users = []
+
     const username = document.getElementById('user').value;
     const email = document.getElementById('email').value;
     const description = document.getElementById('description').value;
 
     const result = (`my name is ${username}, of email ${email} & this is my project ${description}`)
-        // alert(result);
 
     // check for empty values 
     if (!username == '' && !email == '' && !description == '') {
-        return result;
+        alert(result)
     } else {
         alert('Please fill all fields!');
     }
 
     // store then in localStorage 
+
 }
