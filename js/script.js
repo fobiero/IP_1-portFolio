2da19440beb8278316e8960a75372d411f64134f
feLycks <felix.obiero@student.moringaschool.com>
2021-11-08 11:01:25 +0300

    check for empy values

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 function getValues() {
+    // get values from suer 
     const username = document.getElementById('user').value;
     const email = document.getElementById('email').value;
     const description = document.getElementById('description').value;
 
     const result = (`my name is ${username}, of email ${email} & this is my project ${description}`)
-    alert(result);
+        // alert(result);
+
+    // check for empty values 
+    if (!username == '' && !email == '' && !description == '') {
+        return result;
+    } else {
+        alert('Please fill all fields!');
+    }
+
+    // store then in localStorage 
 }
